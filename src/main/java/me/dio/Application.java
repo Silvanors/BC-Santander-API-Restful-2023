b596039e5b12c0a011738bd528039cbdeff56730
Silvanors <silvanors@gmail.com>
2023-10-21 20:30:24 -0300

    Subindo projeto API para publicação no Railway

---
+++ src/main/java/me/dio/Application.java
@@ -0,0 +1,18 @@
+package me.dio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+//editor de JSON on line "https://jsoneditoronline.org/"
+//chatGPT para gerar os diagramas de classes UML "chat.openai.com"
+//o código mermaid fornecido pelo chat pode ser usado como gráfico no github utilizando '''...'''
+//pode ser consultado o site "mermaid.js.org" para conhecer mais
+//o deploy será realizado no https://railway.app pela conta do github.
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
